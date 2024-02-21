3a4f75c0dea83961c102bb49e354680821ca6894
domittaa <klodzinska.d@gmail.com>
2024-02-21 12:12:10 +0100

    fix alembic

---
+++ alembic/versions/9da6871d2809_add_new_models.py
@@ -0,0 +1,26 @@
+"""Add new models
+
+Revision ID: 9da6871d2809
+Revises: 
+Create Date: 2024-02-21 11:11:24.922131
+
+"""
+from typing import Sequence, Union
+
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision: str = '9da6871d2809'
+down_revision: Union[str, None] = None
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
