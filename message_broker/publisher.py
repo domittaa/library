a04a42d4fb2de39b1ac801116e5993419429de8a
domittaa <klodzinska.d@gmail.com>
2024-03-26 13:25:14 +0100

    fix publisher

--- message_broker/publisher.py
+++ message_broker/publisher.py
@@ -6,21 +6,27 @@ from aio_pika import DeliveryMode, ExchangeType
 
 async def send(message):
     connection = await aio_pika.connect_robust(
-        "amqp://guest:guest@rabbitmq:5672/",
+        "amqp://guest:guest@127.0.0.1:5672/",
     )
 
     async with connection:
 
         channel = await connection.channel()
+
         exchange = await channel.declare_exchange("test", ExchangeType.FANOUT, durable=True)
+
         queue = await channel.declare_queue("test_queue", durable=True)
         second_queue = await channel.declare_queue("test_second_queue", durable=True)
+
         await queue.bind(exchange)
         await second_queue.bind(exchange)
 
-        print("sending message")
         await exchange.publish(
             aio_pika.Message(body=message.encode(), delivery_mode=DeliveryMode.PERSISTENT),
             routing_key="test",
         )
-        print("message send")
+
+if __name__ == "__main__":
+    while True:
+        message = "Hello World!"
+        asyncio.run(send(message))
--- scheduler/scheduler.py
+++ scheduler/scheduler.py
@@ -13,8 +13,16 @@ async def find_expire_today():
     results = await db.execute(select(Order).where(Order.due_date == datetime.date.today()))
     order = results.scalars().all()
     for order in order:
-        text = f"Your order {order.id} expires today."
+        text = f"Your order expires today."
+        message = {
+            "order_id": order.id,
+            "message": text
+        }
         notification = Notification(text=text, sent_at=datetime.datetime.now(), order_id=order.id)
         await db.commit()
         await db.refresh(notification)
-        asyncio.run(send(text))
+        asyncio.run(send(message))
+
+
+if __name__ == "__main__":
+    asyncio.run(find_expire_today())
